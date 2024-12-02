7d75f8960b99690e6d965822edf19461f2028259
PieStw <piero.stalteri@icloud.com>
2024-12-02 15:10:22 +0100

    aggiunta card

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Card.css";
+
+export default function Card({ languages }) {
+  return (
+    <>
+      <div class="card">
+        <div class="card-body">This is some text within a card body.</div>
+      </div>
+    </>
+  );
+}
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./Main.css";
 import ButtonList from "../buttonList/ButtonList";
 import { languages } from "../../data/programmingLanguages";
+import Card from "../card/Card";
 
 export default function Main() {
   return (
     <>
       <div className="container">
         <ButtonList languages={languages} />
+        <Card languages={languages} />
       </div>
     </>
   );
