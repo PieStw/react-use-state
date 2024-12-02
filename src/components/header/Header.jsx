8db00e6e66e298da6ee321bf458f185933ab457e
PieStw <piero.stalteri@icloud.com>
2024-12-02 14:37:34 +0100

    aggiunto header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.css";
+import React from "react";
+
+export default function Header() {
+  return (
+    <>
+      <header>
+        <h1>Learn Web development</h1>
+      </header>
+    </>
+  );
+}
