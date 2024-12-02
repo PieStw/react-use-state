94ed36c26b0a87fd240af6f872609f1fac6f5172
PieStw <piero.stalteri@icloud.com>
2024-12-02 15:57:58 +0100

    aggiunta logica bottoni

--- src/components/buttonList/ButtonList.jsx
+++ src/components/buttonList/ButtonList.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ButtonList.css";
 import ButtonItem from "./buttonItem/ButtonItem";
 
 export default function ButtonList({ languages, setLanguage }) {
+  const [activeButton, setActiveButton] = useState(null);
+
   function getId(id) {
+    setActiveButton(id);
     setLanguage(id);
   }
 
@@ -12,7 +15,12 @@ export default function ButtonList({ languages, setLanguage }) {
       <div className="button-list">
         {languages.map((language) => (
           <div key={language.id}>
-            <ButtonItem id={language.id} name={language.title} getId={getId} />
+            <ButtonItem
+              id={language.id}
+              name={language.title}
+              getId={getId}
+              isActive={activeButton === language.id}
+            />
           </div>
         ))}
       </div>
--- src/components/buttonList/buttonItem/ButtonItem.jsx
+++ src/components/buttonList/buttonItem/ButtonItem.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ButtonItem.css";
 
-export default function ButtonItem({ id, name, getId }) {
+export default function ButtonItem({ id, name, getId, isActive }) {
+  const type = isActive ? "warning" : "primary";
+
   return (
     <div className="button-item">
       <button
-        className="btn btn-primary"
+        className={"btn btn-" + type}
         onClick={() => {
           getId(id);
         }}
